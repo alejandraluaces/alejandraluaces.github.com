00914fc5caaad3ebdcfb575c04c540e323e63d49
Alejandra Luaces <aluaces@8thlight.com>
2013-08-20 11:21:24 -0400

    Re-did blog layout. Boom.

---
+++ javascripts/nav.js
@@ -0,0 +1,15 @@
+$(function() {
+    var sticky_navigation_offset_top = $('#navbar').offset().top;
+    var sticky_navigation = function(){
+        var scroll_top = $(window).scrollTop();
+        if (scroll_top > sticky_navigation_offset_top) { 
+            $('#navbar').css({ 'position': 'fixed', 'top':0, 'left':0 });
+        } else {
+            $('#navbar').css({ 'position': 'relative' }); 
+        }   
+    };
+    sticky_navigation();
+    $(window).scroll(function() {
+         sticky_navigation();
+    });
+});
